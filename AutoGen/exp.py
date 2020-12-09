e2861a393479cf93420d1ef8bbaadc0cb9ce777c
Florian Hofer <info@florianhofer.it>
2020-12-09 18:24:03 +0100

    Autogen: changed exp to class expWriter

--- AutoGen/exp.py
+++ AutoGen/exp.py
@@ -8,142 +8,153 @@
 # email info@florianhofer.it
 # -----------------------------------------------------------
 
-from test import todo
 from builtins import list
 
 if __name__ == '__main__':
     pass
 
-def createHeader (fileN, testName):
-    """Create the EXP file header"""
+class expWriter:
     
-    print ("EXP header opening " + testName + "...\n")
-    header = "(* @NESTEDCOMMENTS := 'Yes' *)\n(* @PATH := '' *)\n(* @OBJECTFLAGS := '0, 8' *)\n(* @SYMFILEFLAGS := '2048' *)\n"
-    header += "PROGRAM " + testName + "\n"
-    fileN.write (header)
-
-def createFooter (fileN):
-    """Create the EXP file footer"""
-    
-    print ("EXP footer closure...\n")
-    todo()
-    fileN.write("END_PROGRAM\n")    
-    
-def endDeclaration (fileN):
-    """End of declaration header"""
-     
-    print ("EXP end declaration...\n")
-    todo()
-    fileN.write("(* @END_DECLARATION := '0' *)\n")
-
-def writeConstatns (fileN, constants):
-    """Write test values and size constants to file"""
-    
-    print ("export constants to EXP...\n")
-    fileN.write("VAR CONSTANT\n")
-    
-    for c in constants:
-        text = "    " +  constants[c]['Name'] + " : " + constants[c]['Type'] + " := "
-        if type(constants[c]['Value']) != list:
-            text += str(constants[c]['Value']) + ";\n"
-        else:
-            i = 0
-            for v in constants[c]['Value']:
-                i+=1
-                text += "\n        (* Test case" + str(i) + "*)\n"
-                end = False
-                for w in v:
-                    if end:
-                        text += ",\n"
-                    text += "        " + w
-                    end = True
-                    
-                text += ";\n"
-                    
-        fileN.write(text)
-    fileN.write("END_VAR\n")
-    
-def writeVariables (fileN, variables):
-    """Write test variables needed for test execution"""
-    
-    print ("export variables to EXP...\n")
-    fileN.write("VAR\n")
-    
-    for c in variables:
-        text = "    " +  variables[c]['Name'] + " : " + variables[c]['Type']
+    def __init__ (self, fileName, testName):
+        """Initialize EXP writer object"""
+        try:
+            self.testFile = open(fileName + ".txt", "a")
+        except:
+            print ("Unable to open file to write to")
+            return # TODO: Return exception
         
-        if 'Value' in variables[c]:
-            text += " := "
-            if type(variables[c]['Value']) != list:
-                text += str(variables[c]['Value'])
+        self.testName = testName
+
+    def createHeader(self):
+        """Create the EXP POU file header"""
+        
+        print ("EXP header opening " + self.fileName + "...\n")
+        header = "(* @NESTEDCOMMENTS := 'Yes' *)\n(* @PATH := '' *)\n(* @OBJECTFLAGS := '0, 8' *)\n(* @SYMFILEFLAGS := '2048' *)\n"
+        header += "PROGRAM " + self.testName + "\n"
+        self.testFile.write (header)
+    
+    def createFooter(self):
+        """Create the EXP file footer"""
+        
+        print ("EXP POU footer closure...\n")
+        # TODO: 
+        self.testFile.write("END_PROGRAM\n")    
+        
+    def endDeclaration(self):
+        """End of declaration header"""
+         
+        print ("EXP end declaration...\n")
+        # TODO: 
+        self.testFile.write("(* @END_DECLARATION := '0' *)\n")
+    
+    def writeConstatns(self, constants):
+        """Write test values and size constants to file"""
+        
+        print ("export constants to EXP...\n")
+        self.testFile.write("VAR CONSTANT\n")
+        
+        for c in constants:
+            text = "    " +  constants[c]['Name'] + " : " + constants[c]['Type'] + " := "
+            if type(constants[c]['Value']) != list:
+                text += str(constants[c]['Value']) + ";\n"
             else:
                 i = 0
-                for v in variables[c]['Value']:
+                for v in constants[c]['Value']:
                     i+=1
-                    text += "\n        (* Test case" + str(i) + "*)"
+                    text += "\n        (* Test case" + str(i) + "*)\n"
                     end = False
                     for w in v:
                         if end:
                             text += ",\n"
                         text += "        " + w
                         end = True
-                    
-        text += ";\n"
-                    
-        fileN.write(text)
-    fileN.write("END_VAR\n")
-    
-def createStateMachine(fileN, testName, instanceName, typeVar):
-    """Create test state machine, main test execution"""
-    
-    print ("Create state machine for testing...\n")
-    fileN.write('    testInit(' + testName + ', NoOfTests)\n\n')
-    fileN.write('    CASE _tls_ OF\n')
-    fileN.write('    sT_INIT :    (* Reset *)\n')
-    fileN.write('                SysMemSet (ADR(' + instanceName + '), 0, SIZEOF(' + instanceName + '));\n')
-    fileN.write('                testParam(pSteps, SIZEOF(Tests_Values)/NoOfTests/SIZEOF(Tests_Values[_tlt_,1]));\n')
-    fileN.write('\n')
-    fileN.write('    sT_RUN:    (* test run *)\n')
-    fileN.write('               ptTestVars := ADR(Tests_Values[_tlt_,_tlp_]);\n')
-    
-    fileN.write('               ' + instanceName + '(')
-    
-    for v in typeVar[1]:
-        fileN.write('\n                    ' + typeVar[1][v]['Name'] + ' := ptTestVars^.' + typeVar[1][v]['Name'])
+                        
+                    text += ";\n"
+                        
+            self.testFile.write(text)
+        self.testFile.write("END_VAR\n")
         
-    fileN.write('\n                    );\n')
-    fileN.write('\n')
+    def writeVariables(self, variables):
+        """Write test variables needed for test execution"""
+        
+        print ("export variables to EXP...\n")
+        self.testFile.write("VAR\n")
+        
+        for c in variables:
+            text = "    " +  variables[c]['Name'] + " : " + variables[c]['Type']
+            
+            if 'Value' in variables[c]:
+                text += " := "
+                if type(variables[c]['Value']) != list:
+                    text += str(variables[c]['Value'])
+                else:
+                    i = 0
+                    for v in variables[c]['Value']:
+                        i+=1
+                        text += "\n        (* Test case" + str(i) + "*)"
+                        end = False
+                        for w in v:
+                            if end:
+                                text += ",\n"
+                            text += "        " + w
+                            end = True
+                        
+            text += ";\n"
+                        
+            self.testFile.write(text)
+        self.testFile.write("END_VAR\n")
+        
+    def createStateMachine(self, instanceName, typeVar):
+        """Create test state machine, main test execution"""
+        
+        print ("Create state machine for testing...\n")
+        self.testFile.write('    testInit(' + self.testName + ', NoOfTests)\n\n')
+        self.testFile.write('    CASE _tls_ OF\n')
+        self.testFile.write('    sT_INIT :    (* Reset *)\n')
+        self.testFile.write('                SysMemSet (ADR(' + instanceName + '), 0, SIZEOF(' + instanceName + '));\n')
+        self.testFile.write('                testParam(pSteps, SIZEOF(Tests_Values)/NoOfTests/SIZEOF(Tests_Values[_tlt_,1]));\n')
+        self.testFile.write('\n')
+        self.testFile.write('    sT_RUN:    (* test run *)\n')
+        self.testFile.write('               ptTestVars := ADR(Tests_Values[_tlt_,_tlp_]);\n')
+        
+        self.testFile.write('               ' + instanceName + '(')
+        
+        for v in typeVar[1]:
+            self.testFile.write('\n                    ' + typeVar[1][v]['Name'] + ' := ptTestVars^.' + typeVar[1][v]['Name'])
+            
+        self.testFile.write('\n                    );\n')
+        self.testFile.write('\n')
+        
+        for v in typeVar[2]:
+            self.testFile.write('               assertEqualsD (    Value1 := ' + instanceName + '.' + typeVar[2][v]['Name'] +',\n')
+            self.testFile.write('                                  Value2 := ptTestVars^.' + typeVar[2][v]['Name'] + ',\n')
+            self.testFile.write('                                  MaxCnt := 0, Delay := ptTestVars^.testTime);\n\n')
+        
+        self.testFile.write('    sTC_PASS: Pass := TRUE;\n    END_CASE\n')
+        
+    def createTestDUT(self, typeVar):
+        """Create test variable data type for the test parameter table"""
+        
+        print ("Writing data type used for the test to file..")
     
-    for v in typeVar[2]:
-        fileN.write('               assertEqualsD (    Value1 := ' + instanceName + '.' + typeVar[2][v]['Name'] +',\n')
-        fileN.write('                                  Value2 := ptTestVars^.' + typeVar[2][v]['Name'] + ',\n')
-        fileN.write('                                  MaxCnt := 0, Delay := ptTestVars^.testTime);\n\n')
+        self.testFile.write("(* @NESTEDCOMMENTS := 'Yes' *)\n")
+        self.testFile.write("(* @PATH := '' *)\n")
+        self.testFile.write("(* @OBJECTFLAGS := '0, 8' *)\n")
+        self.testFile.write("TYPE Vars" + self.testName + " :\n")
+        self.testFile.write("STRUCT\n")
     
-    fileN.write('    sTC_PASS: Pass := TRUE;\n    END_CASE\n')
+        self.testFile.write('    ' + typeVar[0]['Name'] + ' : ' + typeVar[0]['Type'] + ';\n')
+        
+        for v in typeVar[1]:
+            self.testFile.write('\n    ' + typeVar[1][v]['Name'] + ' : ' + typeVar[1][v]['Type'] + ';')
     
-def createTestDUT(fileN, testName, typeVar):
-    """Create test variable data type for the test parameter table"""
+        self.testFile.write('\n')
+        for v in typeVar[2]:
+            self.testFile.write('\n    ' + typeVar[2][v]['Name'] + ' : ' + typeVar[2][v]['Type'] + ';')
     
-    print ("Writing data type used for the test to file..")
-
-    fileN.write("(* @NESTEDCOMMENTS := 'Yes' *)\n")
-    fileN.write("(* @PATH := '' *)\n")
-    fileN.write("(* @OBJECTFLAGS := '0, 8' *)\n")
-    fileN.write("TYPE Vars" + testName + " :\n")
-    fileN.write("STRUCT\n")
-
-    fileN.write('    ' + typeVar[0]['Name'] + ' : ' + typeVar[0]['Type'] + ';\n')
+        self.testFile.write('\n')
     
-    for v in typeVar[1]:
-        fileN.write('\n    ' + typeVar[1][v]['Name'] + ' : ' + typeVar[1][v]['Type'] + ';')
-
-    fileN.write('\n')
-    for v in typeVar[2]:
-        fileN.write('\n    ' + typeVar[2][v]['Name'] + ' : ' + typeVar[2][v]['Type'] + ';')
-
-    fileN.write('\n')
-
-    fileN.write("END_STRUCT\n")
-    fileN.write("END_TYPE\n\n")
-    fileN.write("(* @END_DECLARATION := '0' *)\n")
+        self.testFile.write("END_STRUCT\n")
+        self.testFile.write("END_TYPE\n\n")
+        self.testFile.write("(* @END_DECLARATION := '0' *)\n")
     
