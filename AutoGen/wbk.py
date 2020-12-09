257b96470a419f728149d9d1a85292f3ebe370e8
Florian Hofer <info@florianhofer.it>
2020-12-09 18:44:29 +0100

    Autogen: changed wbk to class

--- AutoGen/wbk.py
+++ AutoGen/wbk.py
@@ -10,84 +10,112 @@
 
 import test
 
-def getFunctionVars(columns):
-    """Collect the names and types of I/O vaiables in tables"""
-    
-    inputs = {}
-    outputs= {}
-    number = 0
-    outtoin = False
-    coliter = iter(columns)
-    for col in coliter:
-        if col != None :
-            if not col == "#":
-                var = {"Name" : col}
-                try: 
-                    col = next(coliter)
-                except:
-                    print ("end of line!!")
-                    
-                if col != None:
-                    var["Type"] = col
-                
-                if not outtoin :
-                    outputs[number] = var.copy()
-                else:
-                    inputs[number] = var.copy()
-                number += 1
-                var.clear()
-            else:
-                if outtoin:
-                    break
-                outtoin = True
-                number = 0
-    
-    testTime = {"Name" : "testtime", "Type": "DWORD"}
-    return [testTime, inputs, outputs]    
+import xlrd
+import os.path
 
-def scanLine(columns, typeDef):
-    """Read a variable line and create time and I/O value list"""
+class workbook:
     
-    inputs = {}
-    outputs= {}
-    
-    # Time value
-    testTime = columns[0]
-    
-    i = 1
-    cnt = 0
-    for ntype in typeDef[2]:
-        var = {"Value": columns[i], "Type": columns[i+1]}
-        outputs [cnt]= var.copy()
-        var.clear()
-        i+= 2
-        cnt+=1
-    
-    i+=1 # skip the line with hash
-    cnt = 0
-    for ntype in typeDef[1]:
-        var = {"Value": columns[i]}
-        inputs [cnt]= var.copy()
-        var.clear()
-        i+= 2
-        cnt+=1
+    def __init__(self, bookfile):
         
-    return [testTime, inputs, outputs]    
-    
-def readSequence(sh, scanPos, typeDef):
-    """Read a test sequence in spreadsheet, group them into a value set"""
-    
-    while sh.nrows> scanPos :    
-        sequence = {}
-        scanPos += 1
-        cnt = 0
-        print (sh.nrows)
-        while (sh.nrows > scanPos):
-            if (sh.cell(scanPos,1).value == ''):
+        self.wb = xlrd.open_workbook(os.path.join('',bookfile))
+        self.wb.sheet_names()
+        self.sh = self.wb.sheet_by_index(0)
+
+        #Header information
+        testName = self.sh.cell(0,1).value
+        fbName = self.sh.cell(0,4).value
+        instanceName = self.sh.cell(0,6).value
+
+        scanPos = 0
+        
+        # Find beginning of declaration tables
+        while True:
+            state = self.sh.cell(scanPos,0).value
+        
+            if state != "State":
+                scanPos += 1
+            else:
                 break
-            sequence[cnt] = scanLine(sh.row_values(scanPos,1), typeDef)
-            print ("New input found = " + str(sequence[cnt]))
-            scanPos += 1
-            cnt += 1
+    
+    def getFunctionVars(self, columns):
+        """Collect the names and types of I/O vaiables in tables"""
         
-    return sequence
+        inputs = {}
+        outputs= {}
+        number = 0
+        outtoin = False
+        coliter = iter(columns)
+        for col in coliter:
+            if col != None :
+                if not col == "#":
+                    var = {"Name" : col}
+                    try: 
+                        col = next(coliter)
+                    except:
+                        print ("end of line!!")
+                        
+                    if col != None:
+                        var["Type"] = col
+                    
+                    if not outtoin :
+                        outputs[number] = var.copy()
+                    else:
+                        inputs[number] = var.copy()
+                    number += 1
+                    var.clear()
+                else:
+                    if outtoin:
+                        break
+                    outtoin = True
+                    number = 0
+        
+        testTime = {"Name" : "testtime", "Type": "DWORD"}
+        self.scanPos += 1 # next line, start to scan
+        return [testTime, inputs, outputs]    
+    
+    def scanLine(self, columns, typeDef):
+        """Read a variable line and create time and I/O value list"""
+        
+        inputs = {}
+        outputs= {}
+        
+        # Time value
+        testTime = columns[0]
+        
+        i = 1
+        cnt = 0
+        for ntype in typeDef[2]:
+            var = {"Value": columns[i], "Type": columns[i+1]}
+            outputs [cnt]= var.copy()
+            var.clear()
+            i+= 2
+            cnt+=1
+        
+        i+=1 # skip the line with hash
+        cnt = 0
+        for ntype in typeDef[1]:
+            var = {"Value": columns[i]}
+            inputs [cnt]= var.copy()
+            var.clear()
+            i+= 2
+            cnt+=1
+            
+        return [testTime, inputs, outputs]    
+        
+    def readSequence(self, typeDef):
+        """Read a test sequence in spreadsheet, group them into a value set"""
+        
+        while self.sh.nrows> self.scanPos :    
+            sequence = {}
+            self.scanPos += 1
+            cnt = 0
+            print (self.sh.nrows)
+            while (self.sh.nrows > self.scanPos):
+                if (self.sh.cell(self.scanPos,1).value == ''):
+                    break
+                sequence[cnt] = self.scanLine(self.sh.row_values(self.scanPos,1), typeDef)
+                print ("New input found = " + str(sequence[cnt]))
+                self.scanPos += 1
+                cnt += 1
+            
+        return sequence
