99289485b147e90fb5a4f054416139b6ffa13f4d
Florian Hofer <info@florianhofer.it>
2022-04-11 15:32:48 +0200

    Autogen: migrated code in main to new test class

--- AutoGen/test.py
+++ AutoGen/test.py
@@ -10,4 +10,72 @@
 
 class Test:
     """Test object that contains information on a test POU""" 
-    pass    
+
+    testName = ''
+    fbName = ''
+    instanceName = ''
+    constants = {}
+    variables = {}
+    
+    typeVar = {}
+    steps = {}
+    
+    def __init__(self, testName = 'DummyTest', instanceName = 'TestInst', fbName=''):
+        """Intialise new test object"""
+        self.testName = testName 
+        self.instanceName = instanceName
+        self.fbName = fbName
+    
+    def generateVars(self):
+        """Generate -required- variable list to add to the test POU"""
+    
+        variables = {0:{ 'Name': self.instanceName,    'Type': self.fbName}}
+        variables[1] = { 'Name': 'ptrVars', 'Type': 'POINTER TO ' + self.testName + '_vars'}
+        variables[2] = { 'Name': 'i', 'Type': 'INT', 'Value' : "1"}
+    
+        return variables
+
+    def generateConst (self):
+        """Generate constant values for the test
+        Generate values that represent test parameters and test size parameters
+        to add to the test POU"""
+
+        # Collector for the parametrized test values
+        testvars = []
+        seqlen = 0
+        for s in self.steps:
+            testvar = []
+            for v in self.steps[s]:
+                const = "( " + self.typeVar[0]['Name'] + " := " + str(int(self.steps[s][v][0]))
+                for t in self.typeVar[1]:
+                    const += ", " + self.typeVar[1][t]['Name'] + " := " + str((self.steps[s][v][1][t]['Value']))
+        
+                for t in self.typeVar[2]:
+                    const += ", " + self.typeVar[2][t]['Name'] + " := " + str(self.steps[s][v][2][t]['Value'])
+                const += " )"
+                    
+                testvar.append(const)
+    
+            seqlen = max(seqlen, len(testvar))
+            testvars.append(testvar)
+        
+        # Finally, return the constants for test function 
+        constants = {0:{ 'Name': "NoOfTests",    'Type': "USINT", 'Value' : len(testvars)}}
+        constants[1] = { 'Name': "NoOfInputs",    'Type': "USINT", 'Value' : seqlen}
+        constants[2] = { 'Name': "TestVars",    'Type': "ARRAY [1..NoOfTests,1..NoOfInputs] OF Vars" + self.testName, 'Value' : testvars}
+        #print constants
+    
+        return constants
+        # fix len steps to len array    
+
+    def setTestVars(self, typeVar):
+        self.typeVar = typeVar
+    
+    def getTestVars(self):
+        return self.typeVar
+        
+    def setSteps(self, steps):
+        self.steps = steps
+        
+    def getSteps(self):
+        return self.steps
--- AutoGen/testgen.py
+++ AutoGen/testgen.py
@@ -10,50 +10,8 @@
 
 from exp import ExpWriter
 from wbk import Workbook
+from test import Test
 import sys
-
-def generateConst (steps, typeVar, testName):
-	"""Generate constant values for the test
-	Generate values that represent test parameters and test size parameters
-	to add to the test POU"""
-
-	# Collector for the parametrized test values
-	testvars = []
-	seqlen = 0
-	for s in steps:
-		testvar = []
-		for v in steps[s]:
-			const = "( " + typeVar[0]['Name'] + " := " + str(int(steps[s][v][0]))
-			for t in typeVar[1]:
-				const += ", " + typeVar[1][t]['Name'] + " := " + str((steps[s][v][1][t]['Value']))
-	
-			for t in typeVar[2]:
-				const += ", " + typeVar[2][t]['Name'] + " := " + str(steps[s][v][2][t]['Value'])
-			const += " )"
-				
-			testvar.append(const)
-
-		seqlen = max(seqlen, len(testvar))
-		testvars.append(testvar)
-	
-	# Finally, return the constants for test function 
-	constants = {0:{ 'Name': "NoOfTests",	'Type': "USINT", 'Value' : len(testvars)}}
-	constants[1] = { 'Name': "NoOfInputs",	'Type': "USINT", 'Value' : seqlen}
-	constants[2] = { 'Name': "TestVars",	'Type': "ARRAY [1..NoOfTests,1..NoOfInputs] OF Vars" + testName, 'Value' : testvars}
-	#print constants
-
-	return constants
-	# fix len steps to len array
-
-def generateVars(wbk):
-	"""Generate -required- variable list to add to the test POU"""
-	
-	variables = {0:{ 'Name': wbk.instanceName,	'Type': wbk.fbName}}
-	variables[1] = { 'Name': 'ptrVars', 	'Type': 'POINTER TO ' + wbk.testName + '_vars'}
-	variables[2] = { 'Name': 'i', 			'Type': 'INT', 'Value' : "1"}
-	
-	return variables
-
 
 def main(argv):
 
@@ -69,24 +27,27 @@ def main(argv):
 	
 	#iterate though sheets
 	for wbk in wbkiter:
+				
 		# TODO: create multiple tests per sheet, continue on next sheet with new file	
 		testFile = ExpWriter(testName=wbk.testName, fileName=wbk.testName)
+
+		test = Test(wbk.testName, wbk.instanceName, wbk.fbName)
+		# found header, scan for labels.
+		test.setTestVars(wbk.getFunctionVars()) 
+		# build step dictionary
+		test.setSteps(wbk.readSequences(test.getTestVars()))
 		
 		# test file is open, create header and lets scan for data!e
 		testFile.createHeader()
 			
-		# found header, scan for labels.
-		typeVar = wbk.getFunctionVars()
 		
-		# build step dictionary
-		steps = wbk.readSequences(typeVar)
 			
-		testFile.writeConstants(generateConst(steps, typeVar, wbk.testName))
-		testFile.writeVariables(generateVars(wbk))
+		testFile.writeConstants(test.generateConst())
+		testFile.writeVariables(test.generateVars())
 		testFile.endDeclaration()
-		testFile.createStateMachine(wbk.instanceName, typeVar)
+		testFile.createStateMachine(wbk.instanceName, test.getTestVars())
 		testFile.createFooter()
-		testFile.createTestDUT(typeVar)
+		testFile.createTestDUT(test.getTestVars())
 	
 		testFile.close()
 
