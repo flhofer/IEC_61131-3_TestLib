259c92c3eba7d82b689c517d6016ccaaffa43453
Florian Hofer <info@florianhofer.it>
2022-04-21 09:51:39 +0200

    Autogen: added lines for automated import and poll, missing menu

--- AutoGen/src/autogen.py
+++ AutoGen/src/autogen.py
@@ -14,6 +14,8 @@ email info@florianhofer.it
 from exp import ExpWriter
 from wbk import Workbook
 import sys
+from ideInterface import CoDeSysConnect
+from time import sleep
 
 def main(argv):
 
@@ -23,9 +25,11 @@ def main(argv):
 	Read test Workbook and generate test cases/import for the IEC61131-3
 	""" 
 	
+	#TODO: hardcoded XLS
 	workBook = Workbook('test_unit.xls')
 	wbkiter = iter(workBook)
 	
+	testFiles = []
 	#iterate though sheets
 	for sheet in wbkiter:
 				
@@ -36,6 +40,25 @@ def main(argv):
 		
 		with ExpWriter(fileName=test.testName) as testFile:
 			testFile.writeTest(test)
+			testFiles.append(testFile._fileName)
+	
+	#TODO: hardcoded target pro
+	ide = CoDeSysConnect('..\Examples\Sample1.pro')
+	ide.runTest(testFiles)
+	sleep(30) #TODO: more precise timing needed between import/run and DDE
+	
+	ide.connect()
+	sleep(3)
+	print("Polling test results...")
+	try:
+		while True:
+			ide.poll()
+			sleep(1)
+	except KeyboardInterrupt:
+		pass
+
+	print('Exit program')
+	ide.waitIde()
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
